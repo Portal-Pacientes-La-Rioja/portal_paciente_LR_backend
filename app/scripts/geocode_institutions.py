bc51b95fd68705ea40dafa3a80187cb170471dcd
Emmanuel Arias <eamanu@yaerobi.com>
2023-04-21 17:53:09 -0300

    little improve

--- app/scripts/geocode_institutions.py
+++ app/scripts/geocode_institutions.py
@@ -18,14 +18,14 @@ geo = Nominatim(user_agent="portal-paciente-lr")
 
 for address in addresses:
     institution_id = address[0]
-    direction = address[1]
-    location = geo.geocode(direction)
+    direction = address[1] + ", La Rioja, Argentina"
+    location = geo.geocode(direction, timeout=15)
     if location is not None:
         lat = location.latitude
         long = location.longitude
         cursor.execute('UPDATE institutions SET `lat`=%s, `long`=%s WHERE id=%s', (lat, long, institution_id))
         conn.commit()
     else:
-        print('No se pudo obtener la latitud y longitud para el establecimiento con ID', institution_id)
+        print(f'No se pudo obtener la latitud y longitud para el establecimiento con ID {institution_id}, direccion: {direction}')
 
 conn.close()
