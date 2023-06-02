7028927c4529fe4f7d890235ced3e559505d877b
Emmanuel Arias <eamanu@yaerobi.com>
2023-06-02 07:09:15 -0300

    All endpoint of admin now available for superadmin

--- app/models/permission.py
+++ app/models/permission.py
@@ -11,12 +11,19 @@ from app.models.role_permission import RolePermission
 from app.models.user import User
 from app.models.user_role import UserRole
 
-ADMIN_ROUTES = ['adminbyid']
 
-SUPERADMIN_ROUTES = ['Create an user admin', "create_admin", "assign_institutions", "admins","onoffadmin",
-                    'create_message', 'update_message', 'delete_message', 'send_message',
-                    'set_admin_status_to_person', 'get_admin_status', 'Remove a Person',
-                    'Deny access to a Person', 'Accept a Person','List of id_admin_status Person', 'List of persons',"adminbyid",]
+# TODO: Merge these.
+
+ADMIN_ROUTES = ['create_message', 'update_message', 'delete_message', 'send_message',
+                'set_admin_status_to_person', 'get_admin_status', 'Remove a Person',
+                'Deny access to a Person', 'Accept a Person', 'List of id_admin_status Person',
+                'List of id_admin_status Person', 'List of persons']
+
+SUPERADMIN_ROUTES = ['create_message', 'update_message', 'delete_message', 'send_message',
+                'set_admin_status_to_person', 'get_admin_status', 'Remove a Person',
+                'Deny access to a Person', 'Accept a Person', 'List of id_admin_status Person',
+                'List of id_admin_status Person', 'List of persons', 'Create an user admin',
+                "create_admin", "assign_institutions", "admins", "adminbyid", "onoffadmin"]
 
 
 class Permission(Base):
