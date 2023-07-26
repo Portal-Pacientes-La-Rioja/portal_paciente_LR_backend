7ed0a2e346877b10f30501201aa9039ec37d18e7
Cristian Quevedo <cristianquevedocqr@gmail.com>
2023-07-26 15:41:18 -0300

    added validation to check if person exists

--- app/gear/studies/studies.py
+++ app/gear/studies/studies.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 
 from app.gear.studies.config import UPLOAD_DIR
 
+from app.models.person import Person as model_person
 from app.schemas.responses import ResponseNOK, ResponseOK
 
 
@@ -12,7 +13,15 @@ class StudiesController:
     def __init__(self, db: Session):
         self.db = db
 
-    async def upload_study(self, study: UploadFile = File(...)):
+    async def upload_study(self, person_id: int, study: UploadFile = File(...)):
+        # Validating if the person exists
+        existing_person = (
+            self.db.query(model_person).where(model_person.id == person_id).first()
+        )
+
+        if existing_person is None:
+            return ResponseNOK(message=f"Non existent person_id: {str(person_id)}", code=417)
+
         try:
             os.makedirs(UPLOAD_DIR, exist_ok=True)
 
