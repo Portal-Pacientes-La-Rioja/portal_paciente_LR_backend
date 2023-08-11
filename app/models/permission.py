cef0b5e398f271f6a961797855649d903cdfc474
Emmanuel Arias <eamanu@yaerobi.com>
2023-08-11 17:53:14 -0300

    add getadminstatus to normal ADMIN in permissions.py also reformat file

--- app/models/permission.py
+++ app/models/permission.py
@@ -12,20 +12,42 @@ from app.models.user import User
 from app.models.user_role import UserRole
 
 
-ADMIN_ROUTES = ['create_message', 'update_message', 'delete_message', 'send_message',
-                'Deny access to a Person', 'Accept a Person'
-                "Get Admin by id", "adminbyid", 'List of persons']
+ADMIN_ROUTES = [
+    "create_message",
+    "update_message",
+    "delete_message",
+    "send_message",
+    "Deny access to a Person",
+    "Accept a Person" "Get Admin by id",
+    "adminbyid",
+    "List of persons",
+    "get_admin_status",
+]
 
-SUPERADMIN_ROUTES = ['create_message', 'update_message', 'delete_message', 'send_message',
-                     'set_admin_status_to_person', 'get_admin_status', 'Remove a Person',
-                     'Deny access to a Person', 'Accept a Person', 'List of id_admin_status Person',
-                     'List of id_admin_status Person', 'List of persons', 'Create an user admin',
-                     "create_admin", "assign_institutions", "admins", "adminbyid", "onoffadmin",
-                     "change_password_admin"]
+SUPERADMIN_ROUTES = [
+    "create_message",
+    "update_message",
+    "delete_message",
+    "send_message",
+    "set_admin_status_to_person",
+    "get_admin_status",
+    "Remove a Person",
+    "Deny access to a Person",
+    "Accept a Person",
+    "List of id_admin_status Person",
+    "List of id_admin_status Person",
+    "List of persons",
+    "Create an user admin",
+    "create_admin",
+    "assign_institutions",
+    "admins",
+    "adminbyid",
+    "onoffadmin",
+    "change_password_admin",
+]
 
 
 class Permission(Base):
-
     __tablename__ = "permission"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -37,19 +59,20 @@ class Permission(Base):
     def user_is_authorized(username: str, path: str, method: str) -> bool:
         db: Session = SessionLocal()
 
-        permissions = db.query(Permission) \
-            .join(RolePermission, RolePermission.id == RolePermission.id_permission) \
-            .join(UserRole, RolePermission.id_role == UserRole.id_role) \
-            .join(User, UserRole.id_user == User.id and User.username == username) \
+        permissions = (
+            db.query(Permission)
+            .join(RolePermission, RolePermission.id == RolePermission.id_permission)
+            .join(UserRole, RolePermission.id_role == UserRole.id_role)
+            .join(User, UserRole.id_user == User.id and User.username == username)
             .all()
+        )
 
         db.close()
 
         for permission in permissions:
             patterns = [permission.url]
-            pattern = '(?:% s)' % '|'.join(patterns)
+            pattern = "(?:% s)" % "|".join(patterns)
             if re.match(pattern, path) and permission.method.upper() == method.upper():
-
                 if DEBUG_ENABLED:
                     print("---------------------------")
                     print(permission.name)
@@ -68,7 +91,7 @@ class Permission(Base):
         db: Session = SessionLocal()
         routes = request.app.router.routes
 
-        name = ''
+        name = ""
 
         # TODO: Check how this work.
         for route in routes:
